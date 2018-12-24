f7850e2bafbf332a49529c020e267b74c4de57eb
geooo109 <sdi1400109@di.uoa.gr>
2018-12-24 17:47:00 +0200

    fixing the navbar for the homepage

--- index.php
+++ index.php
@@ -21,33 +21,19 @@
 
   <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Services</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+    <link rel="stylesheet" href="css/nav_br_home.css">
+    <div class="topnav" id="myTopnav">
+      <a href="#home" class="active">Αρχική</a>
+      <a href="#foitits">Φοιτητές</a>
+      <a href="#gramaties">Γραμματείες Τμημάτων</a>
+      <a href="#dianomi">Σημεία Διανομής</a>
+      <a href="#vivliothikes">Βιβλιοθήκες</a>
+      <input type="text" placeholder="Search..">
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+      </a>
+    </div>
 
     <!-- Page Content -->
     <div class="container">
